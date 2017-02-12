7f00286cd4022e864f3bbcd32ebc1552f9c71fc0
chrimill <chris@hexial.com>
2017-02-12 10:28:36 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -16,17 +16,17 @@ function getStats(txt) {
 	nLines = input_str;
 	nLines = nLines.split(/\r\n|\r|\n/);
 	
-	//nNonEmptyLines = input_str;
-	//nNonEmptyLines = nNonEmptyLines.replace(" ", "");
-	//nNonEmptyLines = (nNonEmptyLines.match(/\n(?=\n/g) || []).length;
+	nNonEmptyLines = input_str;
+	nNonEmptyLines = nNonEmptyLines.replace(" ", "");
+	nNonEmptyLines = nLines.length - (nNonEmptyLines.match(/\n(?=\n/g)) || []).length;
+	
 	
 	
 	return {
         nChars: nChars.length,
         nWords: nWords.length,
         nLines: nLines.length,
-        //nNonEmptyLines: nNonEmptyLines,
-		nNonEmptyLines: ??
+        nNonEmptyLines: nNonEmptyLines,
         averageWordLength: 3.3,
         maxLineLength: 33,
         palindromes: ["12321", "kayak", "mom"],
